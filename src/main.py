9a38b992fa7412488834907cc3e530fac5b0f841
Astramast <azerqsdfazerqsdg@gmail.com>
2024-07-25 23:10:31 +0200

    Thu Jul 25 23:10:31 CEST 2024

--- src/main.py
+++ src/main.py
@@ -1,28 +1,15 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from MainScreen import MainScreen
+from HistoryScreen import HistoryScreen
+
 
 class MyApp(App):
-    def build(self):
-        # Create a BoxLayout
-        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
-
-        # Create a Label
-        self.label = Label(text="Hello, Kivy!")
-
-        # Create a Button
-        button = Button(text="Click me!")
-        button.bind(on_press=self.on_button_click)
-
-        # Add widgets to the layout
-        layout.add_widget(self.label)
-        layout.add_widget(button)
-
-        return layout
-
-    def on_button_click(self, instance):
-        self.label.text = "Button clicked!"
+	def build(self):
+		sm = ScreenManager()
+		sm.add_widget(MainScreen(0, name='main'))
+		sm.add_widget(HistoryScreen(name='history'))
+		return sm
 
 if __name__ == '__main__':
-    MyApp().run()
+	MyApp().run()
